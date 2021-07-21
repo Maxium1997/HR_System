6ddb274df2bb9cfd25593274c079dd9aa6c06f70
reSetRoot <wei860925@gmail.com>
2021-07-22 02:31:32 +0800

    Only created two class in models.py: 'Company' and 'Department'. Other attributes in these will added soon.

---
+++ company/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Company(models.Model):
+    name
+    person_in_charge
+
+
+class Department(models.Model):
+    name
+    head
